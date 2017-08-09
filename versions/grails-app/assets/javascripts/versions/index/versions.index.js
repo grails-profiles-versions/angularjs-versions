bc91c1e103875fdad754fb72256e1aa8a8daa37f
sdelamo <sergio.delamo@softamo.com>
2017-08-09 15:38:20 +0200

    3.1.0

---
+++ versions/grails-app/assets/javascripts/versions/index/versions.index.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require_self
+//= require_tree services
+//= require_tree controllers
+//= require_tree directives
+//= require_tree templates
+//= require /angular/ui-bootstrap-tpls
+
+angular.module("versions.index", ["versions.core", 'ui.bootstrap.dropdown']);
---
+++ versions/grails-app/assets/javascripts/versions/versions.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /versions/core/versions.core
+//= require /versions/index/versions.index
+
+angular.module("versions", [
+    "versions.core",
+    "versions.index"
+]);
